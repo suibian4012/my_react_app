d803a7bce2bfd4abcf362134b93b3c1813927821
suibian4012 <jingjuan4012@163.com>
2021-02-24 00:26:41 +0800

    main分支修改测试

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ export default class App extends PureComponent {
   // PureComponent内部实现了shouldComponentUpdate,对state和props进行浅比较
   // shouldComponentUpdate和PureComponent同时存在时，只生效shouldComponentUpdate
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //     // 当state和props数据更新时才需要重新渲染
-  //   const nextStateKeys = Object.keys(nextState)
-  //   for (let i = 0; i < nextStateKeys.length; i++) {
-  //     const nextStateKey = nextStateKeys[i]
-  //     if (!this.state.hasOwnProperty(nextStateKey) || this.state[nextStateKey] !== nextState[nextStateKey]
-  //     ) {
-  //       return true
-  //     }
-  //   }
-  //   return false
-  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    // 当state和props数据更新时才需要重新渲染
+    const nextStateKeys = Object.keys(nextState)
+    for (let i = 0; i < nextStateKeys.length; i++) {
+      const nextStateKey = nextStateKeys[i]
+      if (!this.state.hasOwnProperty(nextStateKey) || this.state[nextStateKey] !== nextState[nextStateKey]
+      ) {
+        return true
+      }
+    }
+    return false
+  }
 
   updateData = () => {
     var hasOwnProperty$2 = Object.prototype.hasOwnProperty
